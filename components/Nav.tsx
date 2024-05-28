e6867e9dc11ae00ea102d912982ea61243e57c4b
kiransahu-dev <sahukiran98.ks@gmail.com>
2024-05-28 18:38:36 +0530

    sticky nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Nav = ({open}:Props) => {
   return (
-    <div className="w-[100%] top-0 h-[12vh] bg-[#01204E] shadow-md">
+    <div className="w-[100%] sticky z-[10000] top-0 h-[12vh] bg-[#01204E] shadow-md">
       <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
         <h1 className="flex-[0.6] cursor-pointer text-2xl font-bold text-white">
           Kiran<span className="text-yellow-300">Sahu</span>
