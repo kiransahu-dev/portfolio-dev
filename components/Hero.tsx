8e90bfee61888bf768621d0b760f6fe7f31d9604
kiransahu-dev <sahukiran98.ks@gmail.com>
2024-05-28 18:32:28 +0530

    herosection added

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Particle from "./Particle";
+
+const Hero = () => {
+  return (
+    <div className="h-[88vh] bg-[#01324e] ] bg-cover bg-center ">
+      <Particle />
+      <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center"></div>
+    </div>
+  );
+};
+
+export default Hero;
